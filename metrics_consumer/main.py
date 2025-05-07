2d9f2f7741fd7af2abcaa9c138ba9b0ec933deb5
vcianchi <67361257+vcianchi@users.noreply.github.com>
2025-05-07 11:54:45 +0200

    added main base

---
+++ metrics_consumer/main.py
@@ -0,0 +1,26 @@
+import asyncio
+import nats
+from concurrent.futures._base import TimeoutError
+
+async def main():
+    # Connect to NATS!
+    nc = await nats.connect("localhost:4222")
+
+    
+    # Receive messages on 'foo'
+    sub = await nc.subscribe("metrics.*")
+
+    while True:
+        # Process a message
+        msg = await sub.next_msg()
+        print("Received:", msg)
+        
+    '''       
+    # Make sure all published messages have reached the server
+    await nc.flush()
+
+    # Close NATS connection
+    await nc.close()
+    '''
+if __name__ == '__main__':
+    asyncio.run(main())
